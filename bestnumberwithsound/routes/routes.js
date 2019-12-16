7fb9232b09f6c3d1046cd26e60f47f4d80b7088c
Alexander Orvik <alexaor@stud.ntnu.no>
2019-12-16 21:25:15 +0100

    Move puppeteer block inside route

--- bestnumberwithsound/routes/routes.js
+++ bestnumberwithsound/routes/routes.js
@@ -3,33 +3,32 @@ const puppeteer = require('puppeteer');
 
 const url = 'https://www.nbim.no/no/';
 
-let newsHeadlines = []
-
-puppeteer
-  .launch()
-  .then(browser => browser.newPage())
-  .then(page => {
-    return page.goto(url).then(function() {
-      return page.content();
-    });
-  })
-  .then(html => {
-    const $ = cheerio.load(html);
-    $('#liveNavNumber').each(function() {
-      newsHeadlines.push({
-        number: $(this).text().replace(/ /g, ''),
-      });
-    });
-
-    console.log(newsHeadlines);
-  })
-  .catch(console.error);
-
-
 var appRouter = function (app) {
   app.get("/number", function(req, res) {
-    res.status(200).send(newsHeadlines);
-  });
-}
+  let newsHeadlines = []
+
+  puppeteer
+    .launch()
+    .then(browser => browser.newPage())
+    .then(page => {
+      return page.goto(url).then(function() {
+        return page.content();
+      });
+    })
+    .then(html => {
+      const $ = cheerio.load(html);
+      $('#liveNavNumber').each(function() {
+        newsHeadlines.push({
+          number: $(this).text().replace(/ /g, ''),
+        });
+      });
+      res.status(200).send(newsHeadlines);
+      console.log(newsHeadlines);
+    })
+    .catch(console.error);
+
+
+    });
+  }
 
 module.exports = appRouter;
